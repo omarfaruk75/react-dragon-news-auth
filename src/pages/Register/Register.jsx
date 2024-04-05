fc766be546cbc981bca0eb542ac477ef7639df51
omar faruk <farukomarcpa@gmail.com>
2024-04-05 16:37:14 +0600

    register page added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
 
-const Login = () => {
-
-    const handleLogin = e => {
+const Register = () => {
+    const handleRegister = e => {
         e.preventDefault();
         console.log(e.currentTarget);
         const form = new FormData(e.currentTarget);
+        console.log(form.get('name'));
         console.log(form.get('email'));
+        console.log(form.get('password'));
 
 
 
@@ -17,9 +18,15 @@ const Login = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className="my-4 text-2xl text-center"> Please <span className="font-black">Login</span></h2>
+            <h2 className="my-4 text-2xl text-center"> Please <span className="font-black">Register</span></h2>
             <div className="text-center">
-                <form onSubmit={handleLogin} className="w-2/6 mx-auto border-2 p-4 rounded-xl">
+                <form onSubmit={handleRegister} className="w-2/6 mx-auto border-2 p-4 rounded-xl">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input required type="text" name="name" placeholder="Write your Name" className="input input-bordered" />
+                    </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text">Email</span>
@@ -31,19 +38,16 @@ const Login = () => {
                             <span className="label-text">Password</span>
                         </label>
                         <input name="password" required type="password" placeholder="password" className="input input-bordered" />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
+
                     </div>
                     <div className="form-control mt-6">
-                        <button className="btn btn-primary">Login</button>
+                        <button className="btn btn-primary">Register</button>
                     </div>
                 </form>
-                <p>Do not have an account please <Link className="text-primary" to={'/register'}>Register</Link></p>
+                <p>Already have an account please <Link className="text-primary" to={'/Login'}>Login</Link></p>
             </div>
-
         </div>
     );
 };
 
-export default Login;
+export default Register;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/career">Career</NavLink></li>
         <li><NavLink to="/login">Login</NavLink></li>
+        <li><NavLink to="/register">Register</NavLink></li>
 
     </>
 
